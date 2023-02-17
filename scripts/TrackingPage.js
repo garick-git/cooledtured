783a1aa8cfa1b004034da87d346470eac659e03d
Fayzan Bhatti <fbhatti@stedwards.edu>
2023-02-17 17:31:55 -0600

    Updated the Ordering Page + Added Changes to Tracking Page

--- scripts/TrackingPage.js
+++ scripts/TrackingPage.js
@@ -1,140 +1,75 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibXNjeWhldiIsImEiOiJjbGRjZ2NxMWcwNHlqM3Ftdnk5bWxpNTA2In0.eVb9A1KnXMC8RXlJjetGpw';
-const map = new mapboxgl.Map({
+// Define the route coordinates
+var start = [-97.754717, 30.22837];
+var end = [-97.939990, 29.882080];
+
+// Define the vehicle icon
+var vehicleIcon = document.createElement('div');
+vehicleIcon.className = 'marker';
+vehicleIcon.style.backgroundImage = '../styles/WeGo.png';
+
+// Define the map
+var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-97.753061, 30.22897], // starting position
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: start,
     zoom: 12
 });
-// set the bounds of the map
-const bounds = [
-    [-123.069003, -90],
-    [122.303707, 90]
-];
-map.setMaxBounds(bounds);
 
-// an arbitrary start will always be the same
-// only the end or destination will change
-const start = [-97.753061, 30.22897];//saint edwards at start location
+// Add navigation control to the map
+map.addControl(new mapboxgl.NavigationControl());
 
-// create a function to make a directions request
-async function getRoute(end) {
-    // make a directions request using cycling profile
-    // an arbitrary start will always be the same
-    // only the end or destination will change
-    const query = await fetch(
-        `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-        { method: 'GET' }
-    );
-    const json = await query.json();
-    const data = json.routes[0];
-    const route = data.geometry.coordinates;
-    const geojson = {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-            type: 'LineString',
-            coordinates: route
+// Get the route between the two locations using Mapbox Directions API
+var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start.join(',') + ';' + end.join(',') + '?geometries=geojson&overview=full&access_token=' + mapboxgl.accessToken;
+
+fetch(url).then(function(response) {
+    return response.json();
+}).then(function(data) {
+    // Define coordinates array
+    var coordinatesArray = data.routes[0].geometry.coordinates;
+map.on('load', function(){
+map.addLayer({
+    id: 'route',
+    type: 'line',
+    source: {
+        type: 'geojson',
+        data: {
+            type: 'Feature',
+            geometry: data.routes[0].geometry
         }
-    };
-    // if the route already exists on the map, we'll reset it using setData
-    if (map.getSource('route')) {
-        map.getSource('route').setData(geojson);
+    },
+    paint: {
+        'line-width': 5,
+        'line-color': '#007cbf'
     }
-    // otherwise, we'll make a new request
-    else {
-        map.addLayer({
-            id: 'route',
-            type: 'line',
-            source: {
-                type: 'geojson',
-                data: geojson
-            },
-            layout: {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            paint: {
-                'line-color': '#3887be',
-                'line-width': 5,
-                'line-opacity': 0.75
-            }
-        });
+});
+});
+    // Add the route to the map
+
+
+    // Fit the map to the route
+    var bounds = data.routes[0].geometry.coordinates.reduce(function(bounds, coord) {
+        return bounds.extend(coord);
+    }, new mapboxgl.LngLatBounds(data.routes[0].geometry.coordinates[0], data.routes[0].geometry.coordinates[0]));
+    map.fitBounds(bounds, { padding: 50 });
+
+    // Create the car marker and add it to the map
+    var carMarker = new mapboxgl.Marker({
+        element: vehicleIcon,
+        anchor: 'center'
+    }).setLngLat(start).addTo(map);
+
+    // Animate the car along the route
+    var index = 0;
+    function animateCar() {
+
+        if (index >= coordinatesArray.length) {
+            index = 0
+        }
+        carMarker.setLngLat(coordinatesArray[index]);
+        index++;
+        setTimeout(animateCar, 1);
+
     }
-}
-
-map.on('load', () => {
-    // make an initial directions request that
-    // starts and ends at the same location
-    getRoute(start);
-
-    // Add starting point to the map
-    map.addLayer({
-        id: 'point',
-        type: 'circle',
-        source: {
-            type: 'geojson',
-            data: {
-                type: 'FeatureCollection',
-                features: [
-                    {
-                        type: 'Feature',
-                        properties: {},
-                        geometry: {
-                            type: 'Point',
-                            coordinates: start
-                        }
-                    }
-                ]
-            }
-        },
-        paint: {
-            'circle-radius': 10,
-            'circle-color': '#3887be'
-        }
-    });
-    map.on('click', (event) => {
-        // const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
-        const end = {
-            type: 'FeatureCollecTRhtion',
-            features: [
-                {
-                    type: 'Feature',
-                    properties: {},
-                    geometry: {
-                        type: 'Point',
-                        coordinates: coords
-                    }
-                }
-            ]
-        };
-        if (map.getLayer('end')) {
-            map.getSource('end').setData(end);
-        } else {
-            map.addLayer({
-                id: 'end',
-                type: 'circle',
-                source: {
-                    type: 'geojson',
-                    data: {
-                        type: 'FeatureCollection',
-                        features: [
-                            {
-                                type: 'Feature',
-                                properties: {},
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: coords
-                                }
-                            }
-                        ]
-                    }
-                },
-                paint: {
-                    'circle-radius': 10,
-                    'circle-color': '#f30'
-                }
-            });
-        }
-        getRoute(coords);
-    });
+    animateCar();
 });
